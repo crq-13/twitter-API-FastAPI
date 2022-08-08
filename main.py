a0e886615946b2c0046ea3f06da0ac6c4c1aceaf
Cristian Rojas <cristian@sioma.app>
2022-08-08 15:09:27 -0500

    models_teewts

--- main.py
+++ main.py
@@ -1,48 +1,14 @@
-# Python
-from uuid import UUID
-from typing import Optional
-from datetime import date
 
-#Pydantic
-from pydantic import BaseModel
-from pydantic import EmailStr, Field
 
 # FastAPI
 from fastapi import FastAPI
 
+# Models
+import models
 
 app = FastAPI()
 
 
-# Models
-
-class UserBase(BaseModel):
-    user_id: UUID = Field(...)
-    email: EmailStr = Field(...)
-
-
-class userLogin(UserBase):
-    password: str = Field(
-        ...,
-        min_length=8
-    )
-
-class User(UserBase):
-    first_name: str = Field(
-        ...,
-        min_length=1,
-        max_length=50
-    )
-    last_name: str = Field(
-        ...,
-        min_length=1,
-        max_length=50
-    )
-    birth_date: Optional[date] = Field(default=None)
-
-
-class Tweet(BaseModel):
-    pass
 
 
 @app.get(path=("/"))
